11fc6472ba5fed29bb9aa4449959752b7db542c3
Sophia Sanchez <sophia.l.sanchez@gmail.com>
2015-06-28 10:59:51 -0700

    updated js logic

--- public/main.js
+++ public/main.js
@@ -1,24 +1,23 @@
 var myFirebaseRef = new Firebase("https://npr-platypus.firebaseio.com/");
 
-<!-- User preferences - test example -->
-var media = "music";
-var popularity = "unique";
-var pic = "politics";
+var media = "news";
+var category = "art";
+var subCategory = "architecture";
 
-myFirebaseRef.child("articles").once("value", function(snapshot) {
+myFirebaseRef.child(media).child(category).child(subCategory).once("value", function(snapshot) {
 	var numChildren = snapshot.numChildren();
 	var randInt = Math.floor((Math.random() * numChildren));
 	var i = 0;
-	myFirebaseRef.child("articles").on("child_added", function(snapshot) {
+	myFirebaseRef.child(media).child(category).child(subCategory).on("child_added", function(snapshot) {
 		if(i == randInt) {
-  			appendContent(snapshot.val());
+			console.log(snapshot.val());
+  			appendContent(snapshot.val().embedLink);
   		}
   		i++;
   	});
 });
 
 	
-
 
 
 
