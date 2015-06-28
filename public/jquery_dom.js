e8988171bc2dccf83dd50df4912851ff413c020e
Caitlin <ccmohnike@gmail.com>
2015-06-28 14:30:39 -0700

    Removed neckhack

--- public/jquery_dom.js
+++ public/jquery_dom.js
@@ -2,7 +2,9 @@ function appendContent(url) {
 var storiesElement = $('.stories');
 storiesElement.contents().remove();
 storiesElement.append("<div><iframe src=" + url + " width='100%' height='290' frameborder='0' scrolling='no'></iframe></div>" );
-
+storiesElement.find('iframe').on('load', function() {
+	$('html, body').animate({
+	    scrollTop: storiesElement.offset().top
+	});
+});
 }
-
-
