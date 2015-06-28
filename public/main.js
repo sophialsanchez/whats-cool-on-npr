afb8448f501b32b51636bb6e88349f94a995d2cc
Sophia Sanchez <sophia.l.sanchez@gmail.com>
2015-06-27 18:55:56 -0700

    fixed url hardcode in main.js

--- public/main.js
+++ public/main.js
@@ -2,11 +2,17 @@ console.log("It worked!");
 
 var myFirebaseRef = new Firebase("https://npr-platypus.firebaseio.com/");
 
-# User preferences - test example
+<!-- User preferences - test example -->
 var media = "music";
 var popularity = "unique";
 var pic = "politics";
 
+
 myFirebaseRef.child("article1").on("value", function(snapshot) {
-  alert(snapshot.val());  // Alerts "Hello this..."
+  var url = snapshot.val(); 
+  console.log(url);
+  appendContent(url);
 });
+
+
+
